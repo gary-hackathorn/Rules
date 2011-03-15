fb9a263e00fe2a91435102b9aed7a3172bf3f2ce
unknown <Gary@.(none)>
2011-03-14 20:57:45 -0500

    add global modifying rule

---
+++ src/main/java/org/hack/GlobalModifyingRule.java
@@ -0,0 +1,29 @@
+package org.hack;
+
+import com.google.common.base.Predicate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlobalModifyingRule implements Rule {
+    Predicate<Map<String,Object>> condition;
+    Action action;
+
+    public GlobalModifyingRule(Predicate<Map<String,Object>> condition, Action action) {
+        this.condition = condition;
+        this.action = action;
+    }
+
+    @Override
+    public void evaluate(Map<String, Object> assset, Map<String, Object> globals) {
+        Map<String,Object> map = new HashMap<String,Object>(assset);
+        map.putAll(globals);
+        if(condition.apply(map))
+        {
+            action.evaluate(map);
+            for(String key:assset.keySet())
+                map.remove(key);
+            globals.putAll(map);
+        }
+    }
+}
