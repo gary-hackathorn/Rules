a68d675ae09bc46711c0bd4272f4b9c3cfbd28a5
gary <gary_hackathorn@hotmail.com>
2016-05-22 21:06:21 -0500

    create test using actual objects

--- src/test/java/org/hack/AssetFirstEngineTest.java
+++ src/test/java/org/hack/AssetFirstEngineTest.java
@@ -54,4 +54,24 @@ public class AssetFirstEngineTest extends EasyMockSupport {
 
         globals = new HashMap<String, Object>();
     }
+
+    @Test
+    public void testEngineWithRealObjects() throws Exception {
+        List<Map<String,Object>> assetList = new ArrayList<Map<String, Object>>();
+        Map<String,Object> asset1 = new HashMap<String, Object>();
+        asset1.put("X", 3);
+        asset1.put("Y", 20);
+        asset1.put("result", "na");
+        assetList.add(asset1);
+        Map<String,Object> globals = new HashMap<String, Object>();
+        DefaultEvaluationContext context = new DefaultEvaluationContext(assetList, globals);
+        JexlCondition condition = new JexlCondition("X < Y");
+        JexlAction action = new JexlAction("result = X * Y");
+        List<Rule> rulesList = new ArrayList<Rule>();
+        rulesList.add(new AssetModifyingRule(condition, action));
+
+        AssetFirstEngine engine = new AssetFirstEngine(rulesList);
+        engine.evaluate(context);
+        System.out.println("result = "+asset1.get("result")) ;
+    }
 }
